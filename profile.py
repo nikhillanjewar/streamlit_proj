133934755aeab618f3e54539953e8d06412997fe
Nikhil Lanjewar <105858734+nikhillanjewar@users.noreply.github.com>
2023-05-09 13:05:12 +0530

    Add files via upload

---
+++ profile.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My Profile😎️", page_icon=":tada:", layout="wide")
+
+#--header_section--
+with st.container():
+	st.subheader("Hi, I am Dino James😎️")
+	st.title("Best Singer in DHH or IHH")
+	st.write("i am good musician, composer and rapper in DHH")
+	st.write("[learn more>](https://wikibio.in/dino-james/)") 
